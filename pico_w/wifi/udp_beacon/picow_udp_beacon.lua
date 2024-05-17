167594d092734ec9fa5b3f009c464c1629129960
Remy Blank <remy@c-space.org>
2024-05-17 14:35:31 +0200

    wifi/udp_beacon: Use mlua.time instead of pico.time.

--- pico_w/wifi/udp_beacon/picow_udp_beacon.lua
+++ pico_w/wifi/udp_beacon/picow_udp_beacon.lua
@@ -6,19 +6,18 @@ _ENV = module(...)
 local config = require 'mlua.config'
 local io = require 'mlua.io'
 local mem = require 'mlua.mem'
-local pico = require 'pico'
+local time = require 'mlua.time'
 local cyw43 = require 'pico.cyw43'
 local util = require 'pico.cyw43.util'
 local wifi = require 'pico.cyw43.wifi'
 local lwip = require 'pico.lwip'
 local pbuf = require 'pico.lwip.pbuf'
 local udp = require 'pico.lwip.udp'
-local time = require 'pico.time'
 
 local UDP_PORT = 4444
 local BEACON_MSG_LEN_MAX = 127
 local BEACON_TARGET = "255.255.255.255"
-local BEACON_INTERVAL_MS = 1000
+local BEACON_INTERVAL = 1000 * time.ms
 
 local function run_udp_beacon()
     local pcb<close> = udp.new()
@@ -31,31 +30,28 @@ local function run_udp_beacon()
         local ok, err = pcb:sendto(p, addr, UDP_PORT)
         p:free()
         if not ok then
-            io.printf("Failed to send UDP packet! error=%s\n", err)
+            io.printf("Failed to send UDP packet: %s\n", lwip.err_str(err))
         else
             io.printf("Sent packet %d\n", counter)
             counter = counter + 1
         end
-
-        time.sleep_ms(BEACON_INTERVAL_MS)
+        time.sleep_for(BEACON_INTERVAL)
     end
 end
 
 function main()
     if not (cyw43.init() and lwip.init()) then
-        io.printf("failed to initialize\n")
+        io.printf("Failed to initialize\n")
         return 1
     end
-
     wifi.set_up(cyw43.ITF_STA, true, cyw43.COUNTRY_WORLDWIDE)
-
     io.printf("Connecting to Wi-Fi...\n")
     local ok, msg = util.wifi_connect(config.WIFI_SSID, config.WIFI_PASSWORD,
-                                      cyw43.AUTH_WPA2_AES_PSK, 30000)
+                                      cyw43.AUTH_WPA2_AES_PSK, 30 * time.sec)
     if not ok then
-        io.printf("failed to connect: %s\n", msg)
+        io.printf("Failed to connect: %s\n", msg)
         return 1
     end
-    io.printf("Connected.\n")
+    io.printf("Connected\n")
     run_udp_beacon()
 end
